d2bb033a9d3c7f85ca95fe2f8ba832321427bccc
AlrxDaley <alexander_daley@icloud.com>
2022-09-22 19:01:05 +0100

    Latest

--- pizzeria_app/pizzeria_app/settings.py
+++ pizzeria_app/pizzeria_app/settings.py
@@ -139,7 +139,10 @@ STATIC_URL = "static/"
 
 # Enable WhiteNoise's GZip compression of static assets.
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "board", "static"),
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
