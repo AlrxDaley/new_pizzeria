be74cc7293a742d193db35fe2750d67a8b8fcf20
AlrxDaley <alexander_daley@icloud.com>
2022-09-22 17:45:09 +0100

    Updated trying to deploy to production

--- pizzeria_app/pizzeria_app/settings.py
+++ pizzeria_app/pizzeria_app/settings.py
@@ -137,8 +137,7 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIR = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
